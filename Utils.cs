9d2622cfeefb8cd8306f23fcfab45c85f06df94a
Richard Harkins <rharkins111@gmail.com>
2016-09-23 15:18:29 -0600

    Added code challenge description

--- Utils.cs
+++ Utils.cs
@@ -1,4 +1,19 @@
-﻿namespace Automation_Code_Challenge_6_CSharp
+﻿/*
+
+automation challenge 6 (Crawler):
+
+Write a crawler that will automatically navigate to every page on the site.
+
+Go to https://www.skiutah.com/
+
+Build a crawler that will start at www.skiutah.com and finds every link/page and goes to that page and finds
+other pages it needs to visit.  Remember to not visit the same page twice and to only visit the pages on the domain.
+
+Listed below is the sample code stub for running this test.
+
+*/
+
+namespace Automation_Code_Challenge_6_CSharp
 {
     [TestClass]
     class Utils : SeleniumWebdriverBaseClass
@@ -11,7 +26,6 @@
             verifyPageTitle(URL, Expected_Value);
             // Visit every page of the skiutah.com domain once
             webCrawler();
-
         }
     }
 }
