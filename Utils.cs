31db6eb5def39b241e832d704010c804d8a5eb67
Richard Harkins <rharkins111@gmail.com>
2016-09-23 15:00:24 -0600

    Initial commit

---
+++ Utils.cs
@@ -0,0 +1,17 @@
+﻿namespace Automation_Code_Challenge_6_CSharp
+{
+    [TestClass]
+    class Utils : SeleniumWebdriverBaseClass
+    {
+        [TestMethod]
+        // This method is the main method for launching tests
+        public void TestLauncher()
+        {
+            // Verify Page Title
+            verifyPageTitle(URL, Expected_Value);
+            // Visit every page of the skiutah.com domain once
+            webCrawler();
+
+        }
+    }
+}
